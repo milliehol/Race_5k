cf93904bd6ffa703775cee2772441bf492af2d25
Carla <carla.holcomb25@gmail.com>
2020-03-03 21:58:04 -0500

    Create application_controller.rb

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+      set :public_folder, 'public'
+      set :views, 'app/views'
+      enable :sessions
+      set :session_secret, "secret"
+      use Rack::Flash, sweep: true
+    end
+
+  get '/' do
+    if is_logged_in?
+      redirect '/race'
+    else
+      erb :index
+    end
+  end
+
+  helpers do
+
+   def is_logged_in?
+     !!current_user
+   end
+
+   def current_user
+     @current_user ||= User.find_by(id: session[:user_id])
+   end
+ end
+
+end
