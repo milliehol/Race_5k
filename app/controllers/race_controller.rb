cbba21104c4c2192373aa69109015c0b9303ad34
Carla <carla.holcomb25@gmail.com>
2020-03-09 23:28:23 -0400

    Update race_controller.rb
    fixed typos

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -1,24 +1,24 @@
 class RaceController < ApplicationController
 
-  get '/race' do
+  get '/races' do
     if is_logged_in?
       @race = Race.all
-      erb :'race/index'
+      erb :'races/index'
     else
       redirect '/'
     end
   end
 
-  get '/race/new' do
+  get '/races/new' do
     if is_logged_in?
-      @locations = Location.all
-      erb :'race/new'
+      @cities = City.all
+      erb :'races/new'
     else
       redirect '/'
     end
   end
 
-  post '/race' do
+  post '/races' do
     @user = User.find(session[:user_id])
     @race = @user.race.build(params[:race])
     if params[:race][:race_id] == nil
@@ -31,15 +31,15 @@ class RaceController < ApplicationController
       redirect "/users/#{@user.slug}"
     else
       flash[:message] = "Race was invalid. Please try again."
-      redirect '/race/new'
+      redirect '/races/new'
     end
   end
 
-  get '/race/:id' do
+  get '/races/:id' do
     if is_logged_in?
       @race = Race.find(params[:id])
       if current_user == @race.user
-        erb :'race/show'
+        erb :'races/show'
       else
         flash[:message] = "You do not have permission to edit or delete this race."
         redirect "/users/#{@race.user.slug}"
@@ -49,11 +49,11 @@ class RaceController < ApplicationController
     end
   end
 
-  get '/race/:id/edit' do
+  get '/races/:id/edit' do
     if is_logged_in?
       @race = Race.find(params[:id])
       if current_user == @race.user
-        erb :'race/edit'
+        erb :'races/edit'
       else
         flash[:message] = "You do not have permission to edit or delete this race."
         redirect "/users/#{@race.user.slug}"
@@ -63,7 +63,7 @@ class RaceController < ApplicationController
     end
   end
   
-  post '/race' do
+  post '/races' do
     @user = User.find(session[:user_id])
     @race = @user.race.build(params[:race])
     if params[:race][:city_id] == nil
@@ -76,18 +76,18 @@ class RaceController < ApplicationController
       redirect "/users/#{@user.slug}"
     else
       flash[:message] = "Race was invalid. Please try again."
-      redirect '/race/new'
+      redirect '/races/new'
     end
   end
 
-  post '/race/:id' do
+  post '/races/:id' do
     @race = Race.find(params[:id])
     @race.update(type: params[:type], time: params[:time], food: params[:food])
     flash[:message] = "Successfully updated race."
     redirect "/users/#{@race.user.slug}"
   end
 
-  delete '/race/:id' do
+  delete '/races/:id' do
     @race = Race.find(params[:id])
     @user = @race.user
     if is_logged_in? && current_user == @race.user
