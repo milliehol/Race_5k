03bff9ba867125358ec27fbaaeb445232d8f4eb5
Carla <carla.holcomb25@gmail.com>
2020-03-10 22:30:17 -0400

    Create city.rb

---
+++ app/models/city.rb
@@ -0,0 +1,13 @@
+class Location < ActiveRecord::Base
+  has_many :races
+  has_many :users, through: :races
+  validates_presence_of :name, :state
+
+  def slug
+    self.name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect{|item| item.slug == slug}
+  end
+end
