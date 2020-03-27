4be26a0f2f3d88a9f1dacc728ad829c169ff90bf
Carla Holcomb <carla.programmer@gmail.com>
2020-03-27 19:27:24 +0000

    changed race first table selection from type to date.

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,5 +1,5 @@
 class Race < ActiveRecord::Base
   belongs_to :user
   belongs_to :city
-  validates_presence_of :user_id, :city_id, :type, :time
+  validates_presence_of :user_id, :city_id, :date, :time
 end
