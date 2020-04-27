aac9c777db3d62b2f3d946b01d2d50d35b049177
Carla <carla.holcomb25@gmail.com>
2020-04-27 15:31:26 -0400

    Update users_controller.rb
    updated flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      flash[:message] = "Welcome back #{@user.name}."
       redirect '/races'
     else
       flash[:message] = "Invalid username or password."
